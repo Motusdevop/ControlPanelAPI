38fb04e674528bdb72d2a80c54cf318802ac3b2a
Motusdevop <motusdevop@gmail.com>
2024-02-24 01:38:12 +0300

    add assets

--- src/main.py
+++ src/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from models.models import ButtonModel
+
+import uvicorn
+
+from models.models import ButtonModel, ButtonsStage
 
 app = FastAPI()
 
@@ -11,25 +14,27 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-buttons = {
-    'one': True,
-    'two': False
-}
-
+stage = ButtonsStage(one = False, two = False)
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse(
-        request=request, name="index.html", context=buttons)
+        request=request, name="index.html")
 
 @app.get('/state')
 async def state():
-    return {'one': buttons["one"], 'two': buttons["two"]}
+    return stage
 
 @app.post('/change')
 async def change(data: ButtonModel):
-    buttons[data.id] = bool(data.on)
-    print(data.id, data.on)
+    
+    match data.id:
+        case 'one':
+            stage.one = bool(data.on)
+        case 'two':
+            stage.two = bool(data.on)
 
-    print(buttons)
     return {'status': 'OK'}
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0')
--- src/models/models.py
+++ src/models/models.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 class ButtonModel(BaseModel):
     id: str
     on: bool
+
+class ButtonsStage(BaseModel):
+    one: bool
+    two: bool
